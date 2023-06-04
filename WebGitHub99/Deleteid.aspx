8e7ad924ec6ef1987e99afae308781fdb181908a
Maximosleo <134728581+Maximosleo@users.noreply.github.com>
2023-06-04 04:03:13 +0300

    added delete user by id fucnction

---
+++ WebGitHub99/Deleteid.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Maximos.master" AutoEventWireup="true" CodeFile="Deleteid.aspx.cs" Inherits="Deleteid" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <h1>delete by ID</h1>
+    <form action = "" method ="post" >
+        id for delete:<input type ="text" id ="identification" name ="identification" />
+        <input type ="submit" id="del_id" name="del_id" value ="DELETE" />
+    </form>
+    <br /> <% = userMsg%>
+</asp:Content>
+
+
+
---
+++ WebGitHub99/Deleteid.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Deleteid : System.Web.UI.Page
+
+{
+    public string userMsg;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        string fileName = "Database31.accdb";
+        string tablename = "Table0211";
+        if (Request.Form["del_id"] != null)
+        {
+            string iD = Request.Form["identification"];
+            string sql = "DELETE FROM Table0211 WHERE id = '" + iD  + "'";
+            MyAdoHelper.DoQuery(fileName, sql);
+            userMsg = "the user was deleted";
+        }
+    }
+}
